c6388dec24b13aa51ac39dcc93162d8c85e15136
Alex-Copley-Software <75288748+Alex-copley@users.noreply.github.com>
2022-10-16 12:59:49 -0400

    Added sanity .env data to vercel .env to try to fix build

--- sanity.ts
+++ sanity.ts
@@ -1,6 +1,8 @@
 import { createClient } from "next-sanity"
 import createImageUrlBuilder from "@sanity/image-url"
 
+console.log(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!)
+
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production", 
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
