f64863916e77428edec5ad441e1614ad493143a5
Alex-Copley-Software <75288748+Alex-copley@users.noreply.github.com>
2022-10-16 13:08:39 -0400

    Update sanity.ts

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,7 @@ console.log(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!)
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production", 
-    projectId: "process.env.NEXT_PUBLIC_SANITY_PROJECT_ID"!,
+    projectId: 'process.env.NEXT_PUBLIC_SANITY_PROJECT_ID'!,
     apiVersion: "2021-03-25",
     useCdn: process.env.NODE_ENV === "production",
 };
